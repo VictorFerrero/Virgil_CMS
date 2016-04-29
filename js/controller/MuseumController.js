0bd63588d161a437b3b90c64420d471d1a16175f
unknown <vferrero14@gmail.com>
2016-04-28 22:28:54 -0500

    trying to make network call

--- js/controller/MuseumController.js
+++ js/controller/MuseumController.js
@@ -3,16 +3,62 @@ myApp.controller('MuseumController', ['$scope', '$rootScope', '$http',
       		$rootScope.museum = null;
 
 		  $scope.baseUrl = "http://52.24.10.104/Virgil_Backend_Stage/Virgil_Backend/index.php/museum/";
-		  
+		   $scope.Museums = [
+				  {
+				   id:-1,
+				   museumName:"",
+				   address: "",
+				   museumCity:   "",	  
+				   museumState:  "",
+				   museumZipcode: ""
+				  },
+				  {
+				   id:1,
+				   museumName:"Agricultural Museum",
+				   address: "205 Derry Hill",
+				   museumCity:   "Uncasville",	  
+				   museumState:  "CT",
+				   museumZipcode: "06382"
+				  },
+      		 	  {
+				   id:2, 	  
+				   museumName:"Space Museum",
+				   address: "112 Main St",	  
+				   museumCity:"Fitchburg",
+				   museumState: "Wi",
+				   museumZipcode: "53703"
+				  },
+      		 	  {
+					id:3,  
+				    museumName:"Geology",
+		            address: "303 Some Street",  
+					museumCity:"Stoughton",
+					museumState:"Wi",
+				    museumZipcode: "53706"	  
+				  }
+    		  ]; 
 		  
           $scope.add = function() {
+
+		      errorCallback = function(response) {
+		            var error = response.data.errors; // this is an array 
+		            console.log(error); // see if we have any errors from php script
+		            // also log status codes from server
+		            console.log(response.status);
+		            console.log(response.statusText);
+
+		            // TODO: display error message to the user
+		        }
 
 		        successCallback = function(response) {
 		            // success of call back could still mean that server side 
 		            // error occurred
 		            if(response.data.success == true) {
 		                // we send back the newly created account to the front end
-		                var museumObj = response.data.record;
+		                var arrMuseumObjects = response.data.museums;
+		                var museumObject = arrMuseumObjects[0]; // just choose the first one for now
+		                $scope.Museums = arrMuseumObjects;
+		                $scope.myMuseums = museumObject;
 		                // save the accontObj in $scope
 		                $rootScope.museum = museumObj;
 		                $scope.Museum = museumObj;
@@ -23,59 +69,15 @@ myApp.controller('MuseumController', ['$scope', '$rootScope', '$http',
 		                errorCallback();
 		            }
 		        }
-		      errorCallback = function(response) {
-		            var error = response.data.errors; // this is an array 
-		            console.log(error); // see if we have any errors from php script
-		            // also log status codes from server
-		            console.log(response.status);
-		            console.log(response.statusText);
-
-		            // TODO: display error message to the user
-		        }
+		        /*
 		        var data = new Object();
-			  
-			  $scope.Museums = [
-				  {
-				   id:-1,
-				   museumName:"",
-				   museumStreet: "",
-				   museumCity:   "",	  
-				   museumState:  "",
-				   museumZipcode: ""
-				  },
-				  {
-				   id:1,
-				   museumName:"Agricultural Museum",
-				   museumStreet: "205 Derry Hill",
-				   museumCity:   "Uncasville",	  
-				   museumState:  "CT",
-				   museumZipcode: "06382"
-				  },
-      		 	  {
-				   id:2, 	  
-				   museumName:"Space Museum",
-				   museumStreet: "112 Main St",	  
-				   museumCity:"Fitchburg",
-				   museumState: "Wi",
-				   museumZipcode: "53703"
-				  },
-      		 	  {
-					id:3,  
-				    museumName:"Geology",
-		            museumStreet: "303 Some Street",  
-					museumCity:"Stoughton",
-					museumState:"Wi",
-				    museumZipcode: "53706"	  
-				  }
-    		  ]; 
-			  
-			  
               data.museumName      = $scope.Museum.museumName;
 			  data.museumAccountId = 1;
 			  data.museumAddress   = $scope.Museum.address;
 			  var museumProfileJSO = $scope.addHours();
 			  data.museumProfileJSON = angular.toJson(museumProfileJSO);
-			  $scope.ajax(data, "createMuseum", successCallback, errorCallback);
+			  */
+			  $scope.ajax(data, "getAllMuseums", successCallback, errorCallback);
           };
 		  
 		  $scope.update = function() {
@@ -95,8 +97,8 @@ myApp.controller('MuseumController', ['$scope', '$rootScope', '$http',
 		  }; 
 		  
 		  $scope.change = function() {
-			  $scope.changeEvent = "new value";
-		  };
+			  $rootScope.museum = $scope.Museum;	
+			};
 		  
 		 $scope.ajax = 
      	function($data, $route, $successCallback, $errorCallback) {
