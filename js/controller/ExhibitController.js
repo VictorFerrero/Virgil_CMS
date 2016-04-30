44e7d42582dd9a62196dd76786ac3960ec264d36
unknown <vferrero14@gmail.com>
2016-04-30 02:20:56 -0500

    skp

--- js/controller/ExhibitController.js
+++ js/controller/ExhibitController.js
@@ -1,6 +1,33 @@
 myApp.controller('ExhibitController', ['$scope',
       function($scope) {
           
+        $scope.Exhibits = [
+          {
+           id:-1,
+           museumId: -1,
+           exhibitTitle:"",
+           exhibitDescription: "",
+          
+          },
+          {
+           id:1,
+           exhibitTitle:"Yonce",
+           exhibitDescription: "Boss Bitch",
+          },
+              {
+           id:2,    
+           exhibitTitle:"Cretaceous Period",
+           exhibitDescription: "I think the T-Rex is actually from this period",
+          },
+              {
+          id:3,  
+           exhibitTitle:"Malcolm X",
+           exhibitDescription: "By any means necessary.",
+          }
+          ]; 
+
+
+
           $scope.add = function() {
               $scope.message = "Welcome " + $scope.Exhibit.exhibitTitle;
           };
