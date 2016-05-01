98485a889567da5834ff1ce03118e992bd53e234
unknown <vferrero14@gmail.com>
2016-05-01 05:04:01 -0500

    dealing with $apply in a different way

--- js/controller/MuseumController.js
+++ js/controller/MuseumController.js
@@ -216,15 +216,17 @@ myApp.controller('MuseumController', ['$scope', '$rootScope', '$http',
 		                	if(obj.id == updateMuseum.id) {
 		                		console.log("Museums before");
 		                		console.log($scope.Museums);
-		                		$scope.Museums.splice(i,1);
-		                		$scope.Museums.splice(i,0,updateMuseum);
+		                		$scope.$apply(function() {
+
+		                			$scope.Museums.splice(i,1);
+		                			$scope.Museums.splice(i,0,updateMuseum);
+		                		});
 		                		console.log($scope.Museums);
 		                		break;
 		                	}
 		                }
 		                $scope.Museums.myMuseums = updateMuseum;
 		                $rootScope.currMuseum = updateMuseum;
-		                $scope.$apply();
 		               }
 		            else {
 		                // server did not return error, but something
