5edcf067364f927070669ef1811f02f339bee35a
unknown <vferrero14@gmail.com>
2016-05-01 02:28:36 -0500

    skp

--- js/controller/MuseumController.js
+++ js/controller/MuseumController.js
@@ -214,7 +214,7 @@ myApp.controller('MuseumController', ['$scope', '$rootScope', '$http',
 		                for(i = 0; i < arr.length; i++) {
 		                	var obj = arr[i];
 		                	if(obj.id == updateMuseum.id) {
-		                		Console.log("TRUE");
+		                		console.log("TRUE");
 		                		arr.splice(i, 1, updateMuseum);
 		                		break;
 		                	}
