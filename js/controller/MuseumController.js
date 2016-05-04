b4262ad57dad00e105fe75a342b98745381306a9
unknown <vferrero14@gmail.com>
2016-05-04 12:27:42 -0500

    skp

--- js/controller/MuseumController.js
+++ js/controller/MuseumController.js
@@ -3,7 +3,7 @@
 myApp.controller('MuseumController', ['$scope', '$rootScope', '$http',  '$timeout',
       function($scope, $rootScope, $http, $timeout) {
       	//	$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-      	$http.defaults.headers.post['Content-Type'] = 'undefined';
+      	//$http.defaults.headers.post['Content-Type'] = 'undefined';
       	  $rootScope.currMuseum;
 
 		  $scope.baseUrl = "http://52.24.10.104/Virgil_Backend_Stage/Virgil_Backend/index.php/";
@@ -26,10 +26,6 @@ myApp.controller('MuseumController', ['$scope', '$rootScope', '$http',  '$timeou
 
 */
 
-	$scope.mapChange = function(image) {
-		console.log(image);
-		console.log('hello');
-	};
 		 /* $scope.Museums = [
 				  {
 				   id:-1,
